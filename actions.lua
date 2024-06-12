21f48b46a524354adafbd5e246670d1d06452101
augov <augov278@gmail.com>
2024-06-12 10:40:29 +1000

    Update actions.lua

--- actions.lua
+++ actions.lua
@@ -1,7 +1,11 @@
 local actions = {}
+
+local sp = loadstring(game:HttpGet('https://raw.githubusercontent.com/grayzcale/simplepath/main/src/SimplePath.lua'))
 local player = game.Players.LocalPlayer
 local char = player.Character
 local hum = char.Humanoid
+
+--actions
 
 function actions.reset()
 	hum.Health = 0
@@ -11,5 +15,31 @@ function actions.emote(emote)
 	hum:PlayEmote(emote)
 end
 
+function actions.jump()
+	hum.Jump = true
+end
 
+function actions.walkto(pos)
+	local path = sp.new(char)
+	path:Run(pos)
+	
+	path.Blocked:Connect(function()
+		path:Run(pos)
+	end)
+end
+
+function actions.followplayer(player)
+	local target = game.Players:FindFirstChild(player)
+	local targetroot = target.Character.HumanoidRootPart
+	
+	local path = sp.new(char)
+	
+	path:Run(targetroot)
+
+	path.Blocked:Connect(function()
+		path:Run(targetroot)
+	end)
+end
+
+	
 return actions
