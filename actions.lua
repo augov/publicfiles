f8b1e50f892478901bfbe0b8fceb5b5189b3c034
augov <141982098+augov@users.noreply.github.com>
2024-10-06 18:43:53 +1100

    Update actions.lua

--- actions.lua
+++ actions.lua
@@ -22,38 +22,50 @@ end)
 function actions.reset()
 	getVariables()
 	hum.Health = 0
+	player.CharacterAdded:Wait()
 end
 
 function actions.emote(emote)
 	getVariables()
 	hum:PlayEmote(emote)
+	hum.Animator.AnimationPlayed:Wait()
 end
 
 function actions.jump()
 	getVariables()
 	hum:ChangeState(Enum.HumanoidStateType.Jumping)
+	local jumpstate
+	wait(1) -- fix this
 end
 
 function actions.walkto(x,y,z)
 	getVariables()
 	path:Run(Vector3.new(char.HumanoidRootPart.Position.X,char.HumanoidRootPart.Position.Y,char.HumanoidRootPart.Position.Z)+Vector3.new(x,y,z))
-	
-	path.Blocked:Connect(function()
+
+	local rerun
+	rerun = path.Blocked:Connect(function()
 		path:Run(Vector3.new(char.HumanoidRootPart.Position.X,char.HumanoidRootPart.Position.Y,char.HumanoidRootPart.Position.Z)+Vector3.new(x,y,z))
 	end)
+	
+	path.Reached:Wait()
+	rerun:Disconnect()
 end
 
-function actions.followplayer(player)
+function actions.followplayer()
 	getVariables()
 	local target = game.Players:FindFirstChild(player)
 	local targetroot = target.Character.HumanoidRootPart
 
 	path:Run(targetroot)
-
-	path.Blocked:Connect(function()
+	
+	local rerun
+	rerun = path.Blocked:Connect(function()
 		path:Run(targetroot)
 	end)
+	
+	path.Reached:Wait()
+	rerun:Disconnect()
 end
 
-	
+
 return actions
